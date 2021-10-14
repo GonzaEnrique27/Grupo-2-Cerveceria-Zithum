5e889393c03516b73a89bbfed18bed602e438b7a
rich-romero <rik.romero@gmail.com>
2021-10-14 09:16:06 -0300

    Modularización CSS + Header

---
+++ public/js/header.js
@@ -0,0 +1,6 @@
+function onClickMenu(){
+	document.getElementById("h-menu").classList.toggle("change");
+	document.getElementById("h-nav").classList.toggle("change");
+	
+	document.getElementById("h-menu-bg").classList.toggle("change-bg");
+}

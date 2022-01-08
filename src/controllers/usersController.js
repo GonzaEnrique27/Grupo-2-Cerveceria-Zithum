5986da2ad0cf1b96a39736336317b14e91dbb7c8
kalanchoe18 <marcoaleman18@gmail.com>
2022-01-08 18:37:57 -0300

    register funcional

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,41 @@
+let { getUsers, writeJsonUser} = require('../data/dataBase')
+let bcrypt = require('bcryptjs');
+
 let controller = {
     register: function(req,res){
-        res.render('register')
-    },login: function(req,res){
-        res.render('login')
+        res.render('./users/register')
+    },
+    processRegister : (req,res) =>{
+        let numId = 1
+
+        getUsers.forEach(user => {
+            if(user.id <= numId){
+                numId++
+            }
+        });
+
+        let { name, lastname, email, password, confirmPpassword} = req.body
+
+        
+        let newUser = {
+            id: +numId,
+            name,
+            lastname,
+            email,
+            password : bcrypt.hashSync(password, 10),
+            category: 'user',
+			avatar: req.file ? req.file.filename : 'default-user-img.png'
+        }
+
+        getUsers.push(newUser);
+
+        writeJsonUser(getUsers);
+
+        res.send('Registrado wachin'); //borrar una vez creada la vista de profile user
+        res.redirect(`/users/${numId}`);
+    },
+    login: function(req,res){
+        res.render('./users/login')
     }
 }
 
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-let controller = require('../controllers/usersController')
+let controller = require('../controllers/usersController');
+let uploadFile = require('../middlewares/uploadUserFile');
 
 /* GET home page. */
+/* router.get('/', controller.listUsers); */
+/* router.get('/:id', controller.profile); */
 router.get('/login', controller.login);
+
 router.get('/register', controller.register);
+router.post('/register',uploadFile.single('image'), controller.processRegister);
 
 module.exports = router;

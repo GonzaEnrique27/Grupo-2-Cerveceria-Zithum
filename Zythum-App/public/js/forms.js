8e4da7842707f88664400421fb1d7e4930b4dbef
kalanchoe18 <marcoaleman18@gmail.com>
2022-03-10 17:43:23 -0300

    login y register mas lindos

--- Zythum-App/public/js/forms.js
+++ Zythum-App/public/js/forms.js
@@ -1,11 +1,22 @@
-const $formLogin = document.querySelector('#form-login');
+/* const $formLogin = document.querySelector('#form-login');
 const $createAccount = document.querySelector('.create-account span');
+const $formRegister = document.querySelector('.form-register');
+const $toLogin = document.querySelector('.form-register #to-login');
 const $inputs = $formLogin.elements;
 
 $createAccount.addEventListener('click', (e)=>{
-    $formLogin.style = 'display: none';
-    $formRegister.style = 'display: block';
+    $formLogin.classList.remove('display-flex');
+    $formLogin.classList.add('display-none');
+    $formRegister.classList.remove('display-none');
+    $formRegister.classList.add('display-flex');
 })
+
+$toLogin.addEventListener('click', (e)=>{
+    $formRegister.classList.remove('display-flex');
+    $formRegister.classList.add('display-none');
+    $formLogin.classList.remove('display-none');
+    $formLogin.classList.add('display-flex');
+}) */
 
 function cambiar(){
     let name = document.getElementById('file-upload').files[0].name;
--- Zythum-App/public/js/registerValidatorFront.js
+++ Zythum-App/public/js/registerValidatorFront.js
@@ -5,7 +5,7 @@ const inputs = document.querySelectorAll('#formulario input'); //  Seleccionamos
 
 const expresiones = {               
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{8,12}$/, // 8 a 12 digitos.
+	password: /^.{8,20}$/, // 8 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
 
@@ -19,6 +19,14 @@ const campos = {
   password: false,
   password2: false
 }
+
+const errores = {
+  name: 'Ingrese un nombre válido',
+  lastname: 'Ingrese un apellido válido',
+  email: 'Ingrese un correo válido',
+  password: 'La contraseña tiene que ser de 8 a 12 dígitos.'
+}
+
 console.log(campos)
 // VALIDACION EN CADA UNO DE LOS CAMPOS //
 
@@ -39,13 +47,10 @@ const validarFormulario = (e) => {
 
    case "password":
     validarCampo(expresiones.password, e.target, 'password')
-    validarPassword2()
-
    break;
    
    case "password2":
-     validarPassword2()
-
+    validarPassword2()
    break;
 
  }
@@ -61,18 +66,15 @@ const validarCampo = (expresion, input, campo) => {
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
 		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
 		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-	  campos[campo]= true
-    console.log(campos)
-
+		document.querySelector(`#grupo__${campo} .error`).innerHTML = "";
+	  campos[campo]= true;
   } else {
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
 		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
 		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-    campos[campo]= false
-    console.log(campos)
+		document.querySelector(`#grupo__${campo} .error`).innerHTML = errores[campo];
+    campos[campo]= false;
   }
 }
 
@@ -83,19 +85,23 @@ const validarCampo = (expresion, input, campo) => {
 const validarPassword2 = () =>{
   const inputPassword1 = document.getElementById('password')
   const inputPassword2 = document.getElementById('password2')
-  if (inputPassword1.value !== inputPassword2.value){
+  if ((inputPassword1.value !== inputPassword2.value) || inputPassword2.value.trim() == ''){
     document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
 		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
 		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+    if(inputPassword2.value.trim() == ''){
+      document.querySelector(`#grupo__password2 .error`).innerHTML = 'Este campo es obligatorio';
+    } else {
+      document.querySelector(`#grupo__password2 .error`).innerHTML = "Las contraseñas deben coincidir.";
+    }
 		campos['password2'] = false;
   }else {
     document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
 		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
 		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		document.querySelector(`#grupo__password2 .error`).innerHTML = "";
 		campos['password2'] = true;
 
 }}
@@ -112,16 +118,12 @@ inputs.forEach((input)=>{
 
 formulario.addEventListener('submit', (e) => {
   e.preventDefault()
-                                             
-  const terminos = document.getElementById('notificaciones')
-  if (campos.name && campos.lastname && campos.email && campos.password && terminos.checked){
-    console.log(campos.name)
-    formulario.submit();
 
-    document.querySelectorAll('formulario__grupo-correcto').forEach((icono)=>{
-       icono.classList.remove('formulario__grupo-correcto')
-     })
-     
-   }         
-                
+  const terminos = document.getElementById('notificaciones');
+
+  if(!terminos.checked){
+    document.querySelector('.input-checkbox .error').innerHTML = 'Debe aceptar los términos y condiciones.'
+  } else if (campos.name && campos.lastname && campos.email && campos.password && terminos.checked){
+    formulario.submit();     
+  }         
 })

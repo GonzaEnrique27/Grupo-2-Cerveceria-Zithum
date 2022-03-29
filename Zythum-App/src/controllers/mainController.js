02937b9e325601c3bf08d1e1823f4c04cb9decb7
kalanchoe18 <marcoaleman18@gmail.com>
2022-03-29 02:44:51 -0300

    cosas del home

--- Zythum-App/src/controllers/mainController.js
+++ Zythum-App/src/controllers/mainController.js
@@ -1,8 +1,35 @@
+const db = require('../database/models');
+const { Op } = require('sequelize');
+
+const Products = db.Product;
+const Categories = db.Category;
+const Subcategories = db.Subcategory;
+const Brands = db.Brand;
+const Sizes = db.Size;
+const Tastes = db.Taste;
+
 let controller = {
     home: (req,res)=> {
-        res.render('index', {
-            session: req.session
+        Products.findAll({
+            include: [
+                {association: 'brand'}, 
+                {association: 'size'}, 
+                {association: 'taste'}, 
+                {association: 'subcategory',
+                include: [{association: 'category'}]
+            }],
+            limit: 8
         })
+        .then(products => {
+                res.render('index', {
+                    products,
+                    /* sliderTitle: "Productos relacionados", */
+                    session: req.session
+                })
+            })
+        /* res.render('index', {
+            session: req.session
+        }) */
     }
 }
 
